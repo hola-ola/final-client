7a0bc1efd288eefddd532dc5d23e082bf89999cd
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-06-23 23:50:49 +0200

    add edit listing styling

--- src/pages/listing/CreateListing.jsx
+++ src/pages/listing/CreateListing.jsx
@@ -7,6 +7,7 @@ import useForm from "../../hooks/useForm";
 
 import "./CreateListing.css";
 import "../../App.css";
+import "../../style/Input.css";
 
 export default function CreateListing(props) {
   const [
@@ -46,60 +47,70 @@ export default function CreateListing(props) {
   return (
     <div className="create-listing-wrapper">
       <h1>Add your listing here</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <p>Title</p>
-          <input {...inputProps("title")} />
-          {error?.key === "title" && (
-            <p className="errorMessage">{error.message}</p>
-          )}
-        </div>
-
-        <div>
-          <p>Country</p>
-          <input {...inputProps("country")} />
-        </div>
-
-        <div>
-          <p>City</p>
-          <input {...inputProps("city")} />
-        </div>
-
-        <div>
-          <p>Possible length of stay</p>
-          <div className="checkbox-container">
-            {AMENITIES.LENGTH_OF_STAY.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("lengthOfStay", { item })}
-              />
-            ))}
+      <form onSubmit={onSubmit} className="create-listing-form">
+        <div className="create-listing-section">
+          <div>
+            <h4>Title</h4>
+            <input
+              className="input-field listing-input-field green"
+              {...inputProps("title")}
+            />
+            {error?.key === "title" && (
+              <p className="errorMessage">{error.message}</p>
+            )}
+          </div>
+          <div>
+            <h4>Country</h4>
+            <input
+              className="input-field listing-input-field green"
+              {...inputProps("country")}
+            />
+          </div>
+          <div>
+            <h4>City</h4>
+            <input
+              className="input-field listing-input-field green"
+              {...inputProps("city")}
+            />
           </div>
         </div>
 
-        <div>
-          <p>Type</p>
-          <div className="checkbox-container">
-            {AMENITIES.LISTING_TYPE.map((item, index) => (
-              <CheckboxInput key={index} {...inputProps("type", { item })} />
-            ))}
+        <div className="create-listing-section">
+          <div>
+            <h4>Possible length of stay</h4>
+            <div className="checkbox-container">
+              {AMENITIES.LENGTH_OF_STAY.map((item, index) => (
+                <CheckboxInput
+                  key={index}
+                  {...inputProps("lengthOfStay", { item })}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h4>Type</h4>
+            <div className="checkbox-container">
+              {AMENITIES.LISTING_TYPE.map((item, index) => (
+                <CheckboxInput key={index} {...inputProps("type", { item })} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h4>Number of sleeping spots</h4>
+            <div className="checkbox-container">
+              {AMENITIES.SLEEPS.map((item, index) => (
+                <CheckboxInput
+                  key={index}
+                  {...inputProps("numberOfSleepingSpots", { item })}
+                />
+              ))}
+            </div>
           </div>
         </div>
 
-        <div>
-          <p>Number of sleeping spots</p>
-          <div className="checkbox-container">
-            {AMENITIES.SLEEPS.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("numberOfSleepingSpots", { item })}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <p>Kitchen equipment</p>
+        <div className="create-listing-section">
+          <h4>Kitchen equipment</h4>
           <div className="checkbox-container">
             {AMENITIES.KITCHEN_EQUIPMENT.map((item, index) => (
               <CheckboxInput
@@ -108,134 +119,131 @@ export default function CreateListing(props) {
               />
             ))}
           </div>
-        </div>
-
-        <div>
-          <p>Bathroom equipment</p>
-          <div className="checkbox-container">
-            {AMENITIES.BATHROOM_EQUIPMENT.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("bathroomEquipment", { item })}
-              />
-            ))}
+          <div>
+            <h4>Bathroom equipment</h4>
+            <div className="checkbox-container">
+              {AMENITIES.BATHROOM_EQUIPMENT.map((item, index) => (
+                <CheckboxInput
+                  key={index}
+                  {...inputProps("bathroomEquipment", { item })}
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h4>Work setup</h4>
+            <div className="checkbox-container">
+              {AMENITIES.WORK_SETUP.map((item, index) => (
+                <CheckboxInput
+                  key={index}
+                  {...inputProps("workSetup", { item })}
+                />
+              ))}
+            </div>
+            <div>
+              <h4>Accessibility</h4>
+              <div className="checkbox-container">
+                {AMENITIES.ACCESSIBILITY.map((item, index) => (
+                  <CheckboxInput
+                    key={index}
+                    {...inputProps("accessibility", { item })}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
 
-        <div>
-          <p>Work setup</p>
-          <div className="checkbox-container">
-            {AMENITIES.WORK_SETUP.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("workSetup", { item })}
-              />
-            ))}
+        <div className="create-listing-section">
+          <div>
+            <h4>Is smoking allowed?</h4>
+            <div className="radio-container">
+              <RadioInput
+                {...inputProps("smokersWelcome", {
+                  value: true,
+                  checked: form.smokersWelcome,
+                })}
+              >
+                Yes
+              </RadioInput>
+              <RadioInput
+                {...inputProps("smokersWelcome", {
+                  value: false,
+                  checked: !form.smokersWelcome,
+                })}
+              >
+                No
+              </RadioInput>
+            </div>
+          </div>
+          <div>
+            <h4>Are kids welcome?</h4>
+            <div className="radio-container">
+              <RadioInput
+                {...inputProps("kidsWelcome", {
+                  value: true,
+                  checked: form.kidsWelcome,
+                })}
+              >
+                Yes
+              </RadioInput>
+              <RadioInput
+                {...inputProps("kidsWelcome", {
+                  value: false,
+                  checked: !form.kidsWelcome,
+                })}
+              >
+                No
+              </RadioInput>
+            </div>
+          </div>
+
+          <div>
+            <h4>Are pets welcome?</h4>
+            <div className="radio-container">
+              <RadioInput
+                {...inputProps("petsWelcome", {
+                  value: true,
+                  checked: form.petsWelcome,
+                })}
+              >
+                Yes
+              </RadioInput>
+              <RadioInput
+                {...inputProps("petsWelcome", {
+                  value: false,
+                  checked: !form.petsWelcome,
+                })}
+              >
+                No
+              </RadioInput>
+            </div>
+          </div>
+          <div>
+            <h4>Do you have space outside (balcony, terrace, garden)?</h4>
+            <div className="radio-container">
+              <RadioInput
+                {...inputProps("spaceOutside", {
+                  value: true,
+                  checked: form.spaceOutside,
+                })}
+              >
+                Yes
+              </RadioInput>
+              <RadioInput
+                {...inputProps("spaceOutside", {
+                  value: false,
+                  checked: !form.spaceOutside,
+                })}
+              >
+                No
+              </RadioInput>
+            </div>
           </div>
         </div>
 
-        <div>
-          <p>Accessibility</p>
-          <div className="checkbox-container">
-            {AMENITIES.ACCESSIBILITY.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("accessibility", { item })}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <p>Is smoking allowed?</p>
-          <div className="radio-container">
-            <RadioInput
-              {...inputProps("smokersWelcome", {
-                value: true,
-                checked: form.smokersWelcome,
-              })}
-            >
-              Yes
-            </RadioInput>
-            <RadioInput
-              {...inputProps("smokersWelcome", {
-                value: false,
-                checked: !form.smokersWelcome,
-              })}
-            >
-              No
-            </RadioInput>
-          </div>
-        </div>
-
-        <div>
-          <p>Are kids welcome?</p>
-          <div className="radio-container">
-            <RadioInput
-              {...inputProps("kidsWelcome", {
-                value: true,
-                checked: form.kidsWelcome,
-              })}
-            >
-              Yes
-            </RadioInput>
-            <RadioInput
-              {...inputProps("kidsWelcome", {
-                value: false,
-                checked: !form.kidsWelcome,
-              })}
-            >
-              No
-            </RadioInput>
-          </div>
-        </div>
-
-        <div>
-          <p>Are pets welcome?</p>
-          <div className="radio-container">
-            <RadioInput
-              {...inputProps("petsWelcome", {
-                value: true,
-                checked: form.petsWelcome,
-              })}
-            >
-              Yes
-            </RadioInput>
-            <RadioInput
-              {...inputProps("petsWelcome", {
-                value: false,
-                checked: !form.petsWelcome,
-              })}
-            >
-              No
-            </RadioInput>
-          </div>
-        </div>
-
-        <div>
-          <p>Do you have space outside (balcony, terrace, garden)?</p>
-          <div className="radio-container">
-            <RadioInput
-              {...inputProps("spaceOutside", {
-                value: true,
-                checked: form.spaceOutside,
-              })}
-            >
-              Yes
-            </RadioInput>
-            <RadioInput
-              {...inputProps("spaceOutside", {
-                value: false,
-                checked: !form.spaceOutside,
-              })}
-            >
-              No
-            </RadioInput>
-          </div>
-        </div>
-
-        <div>
-          <p>General description</p>
+        <div className="create-listing-section">
+          <h4>General description</h4>
           <p>
             Please tell us a bit more about your place and your flatmates (if
             you have any).
@@ -247,36 +255,36 @@ export default function CreateListing(props) {
           />
         </div>
 
-        <div>
-          {/* we should make it interactive labels instead of checkmarks! :D */}
-          <p>What describes your neighborhood best?</p>
-          <div className="checkbox-container">
-            {AMENITIES.AMBIENCE.map((item, index) => (
-              <CheckboxInput
-                key={index}
-                {...inputProps("ambienceLabels", { item })}
-              />
-            ))}
+        <div className="create-listing-section">
+          <div>
+            <h4>What describes your neighborhood best?</h4>
+            <div className="checkbox-container">
+              {AMENITIES.AMBIENCE.map((item, index) => (
+                <CheckboxInput
+                  key={index}
+                  {...inputProps("ambienceLabels", { item })}
+                />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h4>What else shall we know about your area?</h4>
+            <p>Let other users know about the coolest places in the hood!</p>
+            <textarea
+              {...inputProps("extraRemarks", { type: "textarea" })}
+              className="textarea"
+              placeholder="Add your description here"
+            />
           </div>
         </div>
 
-        <div>
-          <p>What else shall we know about your area?</p>
-          <p>Let other users know about the coolest places in the hood!</p>
-          <textarea
-            {...inputProps("extraRemarks", { type: "textarea" })}
-            className="textarea"
-            placeholder="Add your description here"
-          />
-        </div>
-
-        <div>
-          <p>Add images</p>
+        <div className="create-listing-section">
+          <h2>Add images</h2>
           <input type="file" onChange={handleImageChange} />
         </div>
 
-        <div>
-          <p>Is the place available now?</p>
+        <div className="create-listing-section">
+          <h4>Is the place available now?</h4>
           <div className="radio-container">
             <RadioInput
               {...inputProps("availability", {
@@ -296,7 +304,9 @@ export default function CreateListing(props) {
             </RadioInput>
           </div>
         </div>
-        <button>Submit the listing</button>
+        <div className="create-listing-section">
+          <button className="button darkcyan">Submit the listing</button>
+        </div>
       </form>
     </div>
   );
